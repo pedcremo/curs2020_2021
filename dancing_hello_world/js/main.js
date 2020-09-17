ad4784f1fcc23730f8c4fd2222664879e122a6d3
nicklaus_ <pedcremo@gmail.com>
2020-09-17 16:23:46 +0200

    Separated concerns:html, js, css

---
+++ dancing_hello_world/js/main.js
@@ -0,0 +1,25 @@
+let speed = 15; //1 to 100
+
+let incX = speed * (Math.round(Math.random())?1:-1);
+let incY = speed * (Math.round(Math.random())?1:-1);
+let ball=document.getElementById("ball");
+
+function danceWorld(){
+    
+    x =  ball.style.left?parseInt(ball.style.left,10):0;
+    y =  ball.style.top?parseInt(ball.style.top,10):0;
+
+    ball.style.left =  x + incX +"px";
+    ball.style.top = y + incY+"px";
+    
+    //Detect if we reach X coordinates limits
+    if (((x+incX) > (window.innerWidth-40)) || ((x+incX)<=0))
+       incX = (-1)*incX
+       
+    //Detect if we reach Y coordinates limits
+    if (((y+incY) > (window.innerHeight-40)) || ((y+incY) <= 0))
+       incY = (-1)*incY
+
+
+}
+setInterval(danceWorld,50);
