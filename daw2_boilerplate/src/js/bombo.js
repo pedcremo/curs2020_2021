c9a6cb6156eaafc4ab334c688824837252289410
nicklaus_ <pedcremo@gmail.com>
2020-09-29 20:33:47 +0200

    bombo created

--- daw2_boilerplate/src/js/bombo.js
+++ daw2_boilerplate/src/js/bombo.js
@@ -1,26 +1,14 @@
-class Bombo{
+class Bombo{    
     constructor(){
-        this.boles = Array.from({length:99},(_,i) => i + 1);
-        this.bolesExtracted = [];
-    }
-    shuffle(){
-        this.boles.sort((a,b) => Math.random()-0.5);
-    }    
-    pickNumber(){        
-        if (this.boles.length>0){
-            let indexPicked = Math.floor(Math.random()*this.boles.length);
-            let selectedNumber = this.bombo.splice(indexPicked,1);
-            this.bolesExtracted.push(selectedNumber[0]);        
-            return selectedNumber[0];
-        }else{
-            return false;
-        }
-    }
-    getExtractedNumbers(){
-        console.log(this.bolesExtracted);
-    }
-    toString(){
-        console.log(this.boles);
-    }
+        let boles = Array.from({length:90},(_,i) => i + 1);
+        let bolesExtracted = [];
+        let shuffle = () => boles.sort((a,b) => Math.random()-0.5);         
+        this.getExtractedNumbers= () => console.log(bolesExtracted);
+        this.toString = () =>console.log(boles);
+        this.pickNumber = () => {
+            shuffle();             
+            boles[0] && bolesExtracted.push(boles[0]);            
+            return (boles.length>0 && boles.splice(0,1))?bolesExtracted[bolesExtracted.length-1]:false;            
+        }        
+    }   
 }
-
