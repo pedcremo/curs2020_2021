e70263cba9de29c4bfc4e4a49cca5b4972ee7c22
nicklaus_ <pedcremo@gmail.com>
2020-10-08 18:00:53 +0200

    work in progress

--- daw2_boilerplate/src/index.js
+++ daw2_boilerplate/src/index.js
@@ -3,26 +3,25 @@ import './css/style.css';
 import {docReady} from './js/core/core.js'; 
 import './js/card.js';
 import {Bombo} from './js/bombo.js';
-import {generateBingoCard,renderBingoCard} from './js/card.js';
+import {BingoCard} from './js/card.js';
 
 let app = (() => {
     //let el = document.getElementById("ball");
     let myApp;
     let bombo;
-    let myCard;
+    let cardPlayer1,cardPlayer2;
     let stateApp="stop"
     
-    let play =  () =>{    
+    let play = () =>{    
         let num=bombo.pickNumber();
-        if (num){
-            //document.getElementById('balls').innerHTML = "<h1>"+bombo.getExtractedNumbers()+"</h1>";
+        if (num){           
             let ballDiv = document.createElement('div');
             ballDiv.className = 'bingoBall';
             ballDiv.textContent = num;
-            document.getElementById('balls').appendChild(ballDiv);
-            //innerHTML = "<h1>"+bombo.getExtractedNumbers()+"</h1>";
-            document.getElementById('bingoCard').innerHTML = renderBingoCard(myCard,bombo.getExtractedNumbers());
-            //document.getElementById('bingoCard').innerHTML = renderBingoCard(generateBingoCard());
+            document.getElementById('balls').appendChild(ballDiv);           
+            //document.getElementById('bingoCard').innerHTML = renderBingoCard(cardPlayer1,bombo.getExtractedNumbers());
+            document.getElementById('bingoCard1').innerHTML = cardPlayer1.render(bombo.getExtractedNumbers());           
+            document.getElementById('bingoCard2').innerHTML = cardPlayer2.render(bombo.getExtractedNumbers());           
         }else{
             stop();
         }
@@ -35,10 +34,11 @@ let app = (() => {
     let start = () => {
         bombo = new Bombo();
         stateApp = "run";
-        myCard = generateBingoCard();
-
-        document.getElementById('bingoCard').innerHTML = renderBingoCard(myCard);
-        myApp = setInterval(play,2000); 
+        cardPlayer1 =  new BingoCard();       
+        document.getElementById('bingoCard1').innerHTML = cardPlayer1.render();
+        cardPlayer2 =  new BingoCard();       
+        document.getElementById('bingoCard2').innerHTML = cardPlayer2.render();
+        myApp = setInterval(play,200); 
     }
 
     return {start: start
--- daw2_boilerplate/src/js/card.js
+++ daw2_boilerplate/src/js/card.js
@@ -1,73 +1,94 @@
-function generateBingoCard(){
-     let templateRow = [0,1,2,3,4,5,6,7,8];
-     let cardMatrix = [[...templateRow],[...templateRow],[...templateRow]];
-     //Transpose matrix to fullfill all cells with random numbers
-     let transposedCardMatrix=transpose(cardMatrix);
-     transposedCardMatrix.forEach((colCard,index) =>{   
-          transposedCardMatrix[index] = getRandomArbitrary(index*10,(index*10)+10,3);
-     });     
-     //Again transpose to get original shape
-     cardMatrix = transpose(transposedCardMatrix);     
-     
-     //Pass an array and we ramdomly pick only an array of 4 elements supposed to be blanks
-     function getBlanks([...ai]){     
-          let iterator=Array.apply(null, Array(ai.length-4));     
-          iterator.forEach((el)=>{
-               ai.splice(Math.floor(Math.random()*ai.length),1);          
-          });             
-          return ai.map((elem) => Math.floor(elem/10))        
-     }
-     /**
-     * Returns count random numbers between min (inclusive) and max (exclusive)
-     */
-     function getRandomArbitrary(min, max,count) {
-          let arr3=[]
-          do{
-               let randN=Math.floor(Math.random() * (max - min) + min);
-               if (!arr3.includes(randN)) arr3.push(randN);
-          }while(arr3.length!=count)     
-          return arr3.sort();               
-     }
+export class BingoCard{   
+    
+     constructor(){
+          let templateRow = [0,1,2,3,4,5,6,7,8];
+          let cardMatrix = [[...templateRow],[...templateRow],[...templateRow]];
+          //Transpose matrix to fullfill all cells with random numbers
+          let transposedcardMatrix=transpose(cardMatrix);
+          transposedcardMatrix.forEach((colCard,index) =>{   
+               transposedcardMatrix[index] = getRandomArbitrary(index*10,(index*10)+10,3);
+          });     
+          //Again transpose to get original shape
+          
+          cardMatrix = transpose(transposedcardMatrix);                            
+                        
+          let row1Blanks=getBlanks(templateRow);//Get four empty cells
+          let row2Blanks=getBlanks(templateRow);//Get four empty cells
+          //Pass two arrays eliminate numbers duplicates on both and from resulting array pick only an array of 4 elements
+          let duplicatesNonSelectable=row1Blanks.filter(function(i){ return row2Blanks.indexOf(i) >= 0; });  
+          let templateRow1 = [...templateRow];
+          duplicatesNonSelectable.forEach((elem)=> templateRow1[elem]=null);
+          let row3Blanks=getBlanks(templateRow1.filter((elem)=> elem!=null));
+          
+          row1Blanks.forEach((elem)=>cardMatrix[0][elem]=null);//Put a null in every empty picked cell row1
+          row2Blanks.forEach((elem)=>cardMatrix[1][elem]=null);//Put a null in every empty picked cell row2
+          row3Blanks.forEach((elem)=>cardMatrix[2][elem]=null);   
+          //return this.cardMatrix;  
+          this.render = (extractedBalls=[]) => {
+               let out="<table class='bingoCard'>"         
+               cardMatrix.forEach((row)=>{
+                    out+="<tr>"
+                    row.forEach((cellValue)=>{
+                    if (cellValue==null){
+                         out+="<th class='nulo'></th>";
+                    }else{
+                         if (extractedBalls && extractedBalls.indexOf(cellValue) >= 0){
+                              out+="<th class='extracted'>"+cellValue+"</th>";                                  
+                         }else{
+                              out+="<th>"+cellValue+"</th>";
+                         }
+                    }
+                    });
+                    out+="</tr>";
+               })
+               out+="</table>";
+               checkBingo(cardMatrix,extractedBalls);   
+               return out;
+          }          
 
-     let row1Blanks=getBlanks(cardMatrix[0]);//Get four empty cells
-     let row2Blanks=getBlanks(cardMatrix[1]);//Get four empty cells
-     //Pass two arrays eliminate numbers duplicates on both and from resulting array pick only an array of 4 elements
-     let duplicatesNonSelectable=row1Blanks.filter(function(i){ return row2Blanks.indexOf(i) >= 0; });  
-     let templateRow1 = [...cardMatrix[2]];
-     duplicatesNonSelectable.forEach((elem)=> templateRow1[elem]=null);
-     let row3Blanks=getBlanks(templateRow1.filter((elem)=> elem!=null));
-     
-     row1Blanks.forEach((elem)=>cardMatrix[0][elem]=null);//Put a null in every empty picked cell row1
-     row2Blanks.forEach((elem)=>cardMatrix[1][elem]=null);//Put a null in every empty picked cell row2
-     row3Blanks.forEach((elem)=>cardMatrix[2][elem]=null);
-     return cardMatrix;
+          
+     }     
+}
+function checkBingo(cardMatrix,extractedBalls){
+     let bingo=true;
+     cardMatrix.forEach((row)=>{
+          let linia = row.filter((val)=> {if (extractedBalls.indexOf(val)<=0) return val }).length
+          if (linia.length==0) alert("linia")
+          else bingo = false;
+     })     
+     if (bingo) alert("BINGO");
+}
+/**
+ * Returns count random numbers between min (inclusive) and max (exclusive)
+*/
+function getRandomArbitrary(min, max,count) {
+     if (min==0) min=1; //Exception first column from 1 to 9
+     if (max==90) max=91; //Exception last column from 80 to 90
+     let arr3=[]
+     do{
+          let randN=Math.floor(Math.random() * (max - min) + min);
+          if (!arr3.includes(randN)) arr3.push(randN);
+     }while(arr3.length!=count)              
+     return arr3.sort();               
 }
 
-function renderBingoCard(cardMatrix,extractedBalls){
- let out="<table class='bingoCard'>"
-
- cardMatrix.forEach((row)=>{
-      out+="<tr>"
-      row.forEach((cellValue)=>{
-          if (cellValue==null){
-               out+="<th class='nulo'></th>";
-          }else{
-               if (extractedBalls && extractedBalls.indexOf(cellValue) >= 0){
-                    out+="<th class='extracted'>"+cellValue+"</th>";     
-               }else{
-                    out+="<th>"+cellValue+"</th>";
-               }
-          }
-      });
-      out+="</tr>";
- })
- out+="</table>";
- return out;
+//Pass an array and we ramdomly pick only an array of 4 elements supposed to be blanks
+function getBlanks([...ai]){     
+     let iterator=Array.apply(null, Array(ai.length-4));     
+     iterator.forEach((el)=>{
+          ai.splice(Math.floor(Math.random()*ai.length),1);          
+     });
+     return ai;             
+     //return ai.map((elem) => Math.floor(elem/10))        
 }
-
 //Transpose a matrix
 function transpose(matrix){
-     return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
+          
+     return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));     
+     
 }
 
-export {generateBingoCard,renderBingoCard};
+
+
+
+//export {generateBingoCard,renderBingoCard};
