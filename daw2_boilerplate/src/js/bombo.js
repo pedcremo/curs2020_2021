c1e4e84bd77514aaa4e612aae168d6ba8f596466
rmenendezdaw <rmenendeziestacio@gmail.com>
2020-10-29 18:48:35 +0100

    Change color of the last ball

--- daw2_boilerplate/src/js/bombo.js
+++ daw2_boilerplate/src/js/bombo.js
@@ -13,8 +13,14 @@ export class Bombo{
             return (boles.length>0 && boles.splice(0,1))?bolesExtracted[bolesExtracted.length-1]:false;            
         }
         let render = (num) => {
-            let tpl_nums = templateBombo.map((item) => bolesExtracted.includes(item)?{num:item,className:'bingoBall'}:{num:item,className:'bingoBallEmpty'})
+            let tpl_nums = templateBombo.map((item) => {
+                if(bolesExtracted.includes(item)){
+                    return (item==num) ? {num:item,className:'bingoBallNew'}:{num:item,className:'bingoBall'};
+                }else{
+                    return {num:item,className:'bingoBallEmpty'};
+                }
+            });
             rootElement.innerHTML = `${tpl_nums.map(a=>  `<div class='${a.className}'>${a.num}</div>`).join("")}`; 
         }
-    }   
-}
+    }
+}   
