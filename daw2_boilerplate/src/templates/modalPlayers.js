6aa714cca6ea9996ca77d336ba04df6e40a85006
jrevertvila <jrevertvila@gmail.com>
2020-11-01 23:23:19 +0100

    Change appearance modalPlayers (main menu), add player on press enter

--- daw2_boilerplate/src/templates/modalPlayers.js
+++ daw2_boilerplate/src/templates/modalPlayers.js
@@ -5,43 +5,53 @@ import '../css/modalPlayers.css';
 export const modalPlayers =()=>{
     const controllers = () => {
         let playersNames = JSON.parse(localStorage.getItem('playersNames')) || [];
+        document.getElementById('remainingPlayersSpan').innerHTML = "Players: "+playersNames.length + "/50 ";
         let addButton=document.getElementById('addplayer');
         if (addButton) {
             let uList=document.getElementById("listPlayers");
             playersNames.forEach((name,index) => {
                 let li=document.createElement('li');
-                li.innerHTML = `<span class='players'>${index+1}</span><p>${name}</p>`;
+                li.innerHTML = `<div><span class='players'>${index+1}</span><p>${name}</p></div><span class="removePlayer">&times;</span>`;
                 li.addEventListener('click',(event) => {
                     li.remove();
                     playersNames=playersNames.filter((item) => item!=name)
                     localStorage.setItem('playersNames',JSON.stringify(playersNames));
+                    document.getElementById('remainingPlayersSpan').innerHTML = "Players: "+playersNames.length + "/50 ";
                 })
                 uList.appendChild(li);
             });
             addButton.addEventListener("click",(event)=>{  
                 let namePlayer=document.getElementById("fname").value;          
                 if (namePlayer) {
+                    document.getElementById('msg--err').innerHTML = "";
                     let li=document.createElement('li');
-                    li.innerHTML = `<span class='players'>${uList.children.length+1}</span><p>${document.getElementById("fname").value}</p>`;
+                    li.innerHTML = `<div><span class='players'>${uList.children.length+1}</span><p>${document.getElementById("fname").value}</p></div><span class="removePlayer">&times;</span>`;
                     uList.appendChild(li);
                     if (window.localStorage){
                         playersNames.push(document.getElementById("fname").value);
                         localStorage.setItem('playersNames',JSON.stringify(playersNames));
-                        console.log(localStorage.getItem('playersNames'));
+                        document.getElementById('remainingPlayersSpan').innerHTML = "Players: "+playersNames.length + "/50 ";
                     }
                     li.addEventListener('click',(event) => {
                         li.remove();
-                        console.log(namePlayer);
-                        var filtered = playersNames.filter(function(e) { return e !== namePlayer })
+                        let filtered = playersNames.filter(function(e) { return e !== namePlayer })
                         localStorage.setItem('playersNames',JSON.stringify(filtered));
-                        console.log(localStorage.getItem('playersNames'));
+                        document.getElementById('remainingPlayersSpan').innerHTML = "Players: "+filtered.length + "/50 ";
                     })
                     document.getElementById("fname").value = "";
                 }else{
-                    document.getElementById('msg--err').innerHTML = "Enter the player's name!"
+                    document.getElementById('msg--err').innerHTML = "\u26A0  Enter the player's name!"
                 }
             })
         }
+
+        //When press enter for add player
+        document.getElementById("fname").addEventListener("keyup", function (event) { //Add player pressing enter in input
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.getElementById("addplayer").click();
+            }
+        });
 
         //Set interval time options =======================
         let inputVal = document.querySelector('#spinner__value');
@@ -63,12 +73,13 @@ export const modalPlayers =()=>{
         let playBtn=document.getElementById('playBtn');
         playBtn.addEventListener('click',function() {
             if (playersNames.length !== 0 && playersNames != undefined){
+                document.getElementById('msg--err').innerHTML = "";
                 let m=document.getElementById('playersForm');
                 m.style.display = "none";       
                 app.speed = (parseFloat(inputVal.value) * 1000);
                 app.start();
             }else{
-                document.getElementById('msg--err').innerHTML = "Add some players first!"
+                document.getElementById('msg--err').innerHTML = "\u26A0  Add some players first!"
             }
         });
         let unmuteBtn=document.getElementById('unmuteBtn');
@@ -90,42 +101,40 @@ export const modalPlayers =()=>{
     `
     <i class="fas fa-volume-mute btn--mute mutted" id="unmuteBtn"></i>
     <div id="playersForm" class="modal">
-            <!-- Modal content -->
-            <div class="modal-content">
-                <span class="close">&times;</span>
-                <h1>Bingo players</h1>
-                <p></p>
-                <div class='players'>
-                <ol id="listPlayers"></ol>
-                </div>                   
-                <div style="display:flex">
-                <input type="text" id="fname" name="fname" placeholder="Player name">                                                
-                <button id='addplayer' class="button">Add</button>
-                </div>
-                <p class="msg--error" id="msg--err"></p>
-                <div class="menu__options">
-                    <button id='playBtn' class="button">PLAY</button>
-                    
-                    <div class="spinner__opts" style="margin-left:10px">
-                        <span> Timer: (sec)</span>
-                        <div class="spinner">
-                            <button type="button" id="spinner__down" class="spinner__btn spinner__down">&lsaquo;</button>
-                            <input type="number" id="spinner__value" class="spinner__value" name="time" id="match_time" min="0.1" max="5" step="0.1" value="0.5">
-                            <button type="button" id="spinner__up" class="spinner__btn spinner__up">&rsaquo;</button>
-                        </div>    
-                    </div>
-                </div>
-            
-            </div>  
-            
-        </div>
-        <div>
-            <video autoplay muted loop id="videoBackground">
-                <source src="${video}" type="video/mp4">
-                Your browser does not support HTML5 video.
-            </video>
-            
-        </div>     
+        <!-- Modal content -->
+        <div class="modal-content ">
+            <span class="close">&times;</span>
+            <h1>BINGO TWINGO</h1>
+            <div class='modal__players__list'>
+            <ol id="listPlayers"></ol>
+            </div>                   
+            <div class="menu__addPlayer">
+                <input type="text" id="fname" name="fname" placeholder="Player name">                                               
+                <i class="far fa-plus-square menu__addPlayer__btn" id='addplayer'></i>
+            </div>
+            <p class="msg--error" id="msg--err"></p>
+            <div class="menu__options">
+                <button id='playBtn' class="menu__start_btn">START GAME</button>
+            </div>
+            <div class="spinner__opts" style="margin-left:10px">
+                <span> Timer: (sec)</span>
+                <div class="spinner">
+                    <button type="button" id="spinner__down" class="spinner__btn spinner__down">&lsaquo;</button>
+                    <input type="number" id="spinner__value" class="spinner__value" name="time" id="match_time" min="0.1" max="5" step="0.1" value="0.5">
+                    <button type="button" id="spinner__up" class="spinner__btn spinner__up">&rsaquo;</button>
+                </div>    
+            </div>
+            <span class="remainingPlayers" id="remainingPlayersSpan"></span>
+        </div>  
+    </div>
+
+    <div>
+        <video autoplay muted loop id="videoBackground">
+            <source src="${video}" type="video/mp4">
+            Your browser does not support HTML5 video.
+        </video>
+        
+    </div>     
                          
        
     
